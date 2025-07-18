f8370424a0443bf7c7c06be3cce2a4b8a4945c3d
nijoe1 <nick@fil.builders>
2025-07-18 17:37:49 +0300

    fix: proofset-details rendered and download root using the sdk

--- utils/getProofset.ts
+++ utils/getProofset.ts
@@ -2,8 +2,7 @@ import { CONTRACT_ADDRESSES, PandoraService } from "@filoz/synapse-sdk";
 import { JsonRpcSigner } from "ethers";
 import { config } from "@/config";
 
-// Pick the provider that has the most used storage
-// in a proofset with the client
+// Returns the providerId and the proofset with the most used storage for the client
 export const getProofset = async (
   signer: JsonRpcSigner,
   network: "mainnet" | "calibration",
@@ -14,29 +13,33 @@ export const getProofset = async (
     CONTRACT_ADDRESSES.PANDORA_SERVICE[network]
   );
   let providerId;
-  let bestProofset;
-  const AllproofSets =
-    await pandoraService.getClientProofSetsWithDetails(address);
+  let mostUtilizedProofset;
+  // Fetch all proofsets for the client
+  const allProofSets = await pandoraService.getClientProofSetsWithDetails(
+    address
+  );
 
-  const proofSetsWithCDN = AllproofSets.filter((proofSet) => proofSet.withCDN);
-
-  const proofSetsWithoutCDN = AllproofSets.filter(
+  // Filter proofsets based on CDN usage
+  const proofSetsWithCDN = allProofSets.filter((proofSet) => proofSet.withCDN);
+  const proofSetsWithoutCDN = allProofSets.filter(
     (proofSet) => !proofSet.withCDN
   );
 
+  // Select proofsets based on config
   const proofSets = config.withCDN ? proofSetsWithCDN : proofSetsWithoutCDN;
 
   try {
-    bestProofset = proofSets.reduce((max, proofSet) => {
+    // Find the proofset with the highest currentRootCount
+    mostUtilizedProofset = proofSets.reduce((max, proofSet) => {
       return proofSet.currentRootCount > max.currentRootCount ? proofSet : max;
     }, proofSets[0]);
-    if (bestProofset) {
+    if (mostUtilizedProofset) {
       providerId = await pandoraService.getProviderIdByAddress(
-        bestProofset.payee
+        mostUtilizedProofset.payee
       );
     }
   } catch (error) {
     console.error("Error getting providerId", error);
   }
-  return { providerId, proofset: bestProofset };
+  return { providerId, proofset: mostUtilizedProofset };
 };
